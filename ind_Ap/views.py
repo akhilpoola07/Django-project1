95ced4b1e53b16dd140f158e14c9d6fc90768d34
akhilpoola07 <akhilreddypoola07@gmail.com>
2026-05-05 15:11:30 +0530

    added the html file

--- ind_Ap/views.py
+++ ind_Ap/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def chittor(request):
-    response = "<h1>Welcome to chittor</h1>"
-    return HttpResponse(response)
+    user_name = "Akhil"
+    age = 21
+    context = { 
+        "name":user_name,
+        "age": age
+        }
+    return render(request,"chittor.html",context)
+    
 def kadapa(request):
     response = "<h1>Welcome to Kadapa</h1>"
     return HttpResponse(response)
